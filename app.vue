f1c8f2405419ab0fab8b10d4b2ee35e6fa6b203f
rngqd <Gorodgorod322@>
2024-10-27 13:45:59 +0300

    Добавление header в общий шаблон

--- app.vue
+++ app.vue
@@ -1,5 +1,15 @@
 <template>
   <NuxtLayout class="relative">
-    <NuxtPage />
+    <AppHeader/>
+    <main class="app-main">
+      <NuxtPage/>
+    </main>
   </NuxtLayout>
 </template>
+
+<style lang="scss">
+.app-main {
+  max-width: 676px;
+  margin: 0 auto;
+}
+</style>
---
+++ components/AppHeader.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <header class="app-header">
+    <div class="app-header__container">
+      <NuxtLink to="/">
+        <h1 class="app-header__title">
+          ikakprosto.ru
+        </h1>
+      </NuxtLink>
+    </div>
+  </header>
+</template>
+
+<style scoped lang="scss">
+@import 'assets/styles/colors';
+
+.app-header {
+  padding: 16px 0 10px;
+}
+
+.app-header__container {
+  height: 76px;
+}
+
+.app-header__title {
+  width: fit-content;
+  margin: 0 auto;
+  text-decoration: none;
+  color: $primary;
+  font-size: 40px;
+}
+</style>
